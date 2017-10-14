1e2b3b88fdb353e794403deff8d263e66a11e10a
Eric Liao <rcliao01@gmail.com>
2017-10-13 21:40:48 -0700

    chore: improve documentation for quiz 2

--- cs4660/quiz/main.py
+++ cs4660/quiz/main.py
@@ -2,6 +2,12 @@
 quiz2!
 
 Use path finding algorithm to find your way through dark dungeon!
+
+Tecchnical detail wise, you will need to find path from node 7f3dc077574c013d98b2de8f735058b4
+to f1f131f647621a4be7c71292e79613f9
+
+TODO: implement BFS
+TODO: implement Dijkstra utilizing the path with highest effect number
 """
 
 import json
@@ -15,23 +21,26 @@ except ImportError:
 GET_STATE_URL = "http://192.241.218.106:9000/getState"
 STATE_TRANSITION_URL = "http://192.241.218.106:9000/state"
 
-def get_state(state_id):
+def get_state(room_id):
     """
-    get state and its neighbor
+    get the room by its id and its neighbor
     """
-    body = {'id': state_id}
-    return json_request(GET_STATE_URL, body)
+    body = {'id': room_id}
+    return __json_request(GET_STATE_URL, body)
 
-def transition_state(state_id, action):
+def transition_state(room_id, next_room_id):
     """
-    transition from the state_id with action (display event)
-    """
-    body = {'id': state_id, 'action': action}
-    return json_request(STATE_TRANSITION_URL, body)
+    transition from one room to another to see event detail from one room to
+    the other.
 
-def json_request(target_url, body):
+    You will be able to get the weight of edge between two rooms using this method
     """
-    helper method to send json request and parse response json
+    body = {'id': room_id, 'action': next_room_id}
+    return __json_request(STATE_TRANSITION_URL, body)
+
+def __json_request(target_url, body):
+    """
+    private helper method to send JSON request and parse response JSON
     """
     req = Request(target_url)
     req.add_header('Content-Type', 'application/json; charset=utf-8')
@@ -42,6 +51,7 @@ def json_request(target_url, body):
     return response
 
 if __name__ == "__main__":
-    empty_room = get_state('10a5461773e8fd60940a56d2e9ef7bf4')
+    # Your code starts here
+    empty_room = get_state('7f3dc077574c013d98b2de8f735058b4')
     print(empty_room)
     print(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']))
